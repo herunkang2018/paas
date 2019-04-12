ddd13f36c48f7e03e635a91b0ae96bc5704e059b
hackeryard <hrunker@gmail.com>
2019-04-12 21:06:41 +0800

    first

---
+++ login/login/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/root/ex/paas/paas/login/login/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ release.md
@@ -0,0 +1,4 @@
+# 蓝鲸智云 PaaS 社区版版本记录
+
+# 3.2.2
+- first version
